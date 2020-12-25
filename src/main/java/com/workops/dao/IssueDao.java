c363208cfcb4dbc14317c7197e76b7836f44ed7d
bhavesh299 <surtyanibhavesh29@gmail.com>
2020-12-25 11:46:18 +0530

    Issue Change

--- src/main/java/com/workops/dao/IssueDao.java
+++ src/main/java/com/workops/dao/IssueDao.java
@@ -1,12 +1,21 @@
 package com.workops.dao;
 
+import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.workops.model.Issue;
 
 public interface IssueDao extends JpaRepository <Issue,String>{
 
 	Optional<Issue> findByName(String name);
+	@Transactional
+	@Modifying
+	@Query(value="SELECT * from issue i where i.sprintid is null",nativeQuery=true)
+	List<Issue>findAllIssues();
 }
